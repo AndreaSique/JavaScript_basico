d023287b94772421a20c9df0067fb21d76abd964
AndreaSique <69050106+AndreaSique@users.noreply.github.com>
2021-02-01 15:31:22 -0600

    Actualización
    Se cargaron los nuevos archivos

---
+++ FORMULARIO/js/script.js
@@ -0,0 +1,21 @@
+//VARIABLES Y TIPOS DE DATOS EN JAVASCRIPT
+var saludo = "Hola JavaScript"
+var edad = 6
+var unPoco = 1.5
+var siNo = true
+var seccion = 'a'
+
+//MOSTRAR UN DATO EN CONSOLO
+console.log(saludo) 
+
+//CONDICIONALES
+
+edad = prompt("Edad")
+
+if(edad >= 18){
+   
+    alert("eres mayor de edad")
+       
+   }else{
+       alert("eres menor de edad")
+   }
