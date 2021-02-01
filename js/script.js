8c800577beaef771fdb8d61f2fa3b84d2a939efe
AndreaSique <69050106+AndreaSique@users.noreply.github.com>
2021-02-01 15:38:37 -0600

    cambios

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 //VARIABLES Y TIPOS DE DATOS EN JAVASCRIPT
 var saludo = "Hola JavaScript"
-var edad = 26
+var edad = 6
 var unPoco = 1.5
 var siNo = true
 var seccion = 'a'
 
-//MOSTRAR UN DATO EN CONSOLA
-console.log(saludo)
+//MOSTRAR UN DATO EN CONSOLO
+console.log(saludo) 
 
+//CONDICIONALES
 
+edad = prompt("Edad")
 
-var datoApellido = document.getElementById('favorito')
+if(edad >= 18){
+   
+    alert("eres mayor de edad")
+       
+   }else{
+       alert("eres menor de edad")
+   }
